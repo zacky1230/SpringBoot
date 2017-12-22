fd1c7edfe56b55bff49f1a08682281070db2fa2b
zhangcheng <zhangcheng@golaxy.cn>
2017-12-22 16:53:50 +0800

    Spring Profile 设置运行环境

---
+++ src/main/java/com/wisely/hightlight_spring4/ch2/profile/Main.java
@@ -0,0 +1,20 @@
+package com.wisely.hightlight_spring4.ch2.profile;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new
+                AnnotationConfigApplicationContext();
+
+        context.getEnvironment().setActiveProfiles("dev");
+        context.register(ProfileConfig.class);
+        context.refresh();
+
+        DemoBean demoBean = context.getBean(DemoBean.class);
+
+        System.out.println(demoBean.getContent());
+
+        context.close();
+    }
+}
